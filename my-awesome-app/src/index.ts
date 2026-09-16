33fd9628e8357a5a610565b76d37ff55a0f94f0d
Ajay <Ajay.dhiman@ditstek.com>
2026-09-16 18:11:28 +0530

    test: add missing import scenario

--- my-awesome-app/src/index.ts
+++ my-awesome-app/src/index.ts
@@ -3,5 +3,9 @@ import { createApp, describeApp } from "./app.js";
 const app = createApp();
 console.log(describeApp(app));
 
+export function getAppDirectory(): string {
+	return join(process.cwd(), app.appName);
+}
+
 export { app };
 
